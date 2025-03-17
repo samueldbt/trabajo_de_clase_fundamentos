74ba0a2e89740c4ee737daf60fc4f9cc9126f312
samueldbt <130416036+samueldbt@users.noreply.github.com>
2025-03-16 20:24:03 -0500

    el 5 pues.py

--- bases_taller.py
+++ bases_taller.py
@@ -36,14 +36,19 @@ while n > 0: #conversion del numero a la base 8
 
 factor = 1
 n = z  
-while n > 0: #conversion del numero a la base 16
-    r = n % d
-    #posicion IGUAL A LA r MAS 1
-    
+while n > 0: #conversion del num ero a la base 16
+    r = n % d #residuo
+
+    if r > 9:
+        b = r - 10
+        r = hex[int(b)]    
     n = float(n) // d
-    m4 = m4 + factor * r
-    factor = factor * 10
-    # n = n (qque es el cociente) concatenado con la posicion en el arreglo
+    m4 = f"{m4 + factor * r}"
+
+m4i = "" #este mini bucle es para invertir la cadena hexadecimal
+for i in m4:
+    m4i = i + m4i
 
 
+print(f"En base 2: {int(m1)}. En base 4: {int(m2)}. En base 8: {int(m3)}. En base 16: {m4i}") # imprime el numero en su version de la base x
 print(f"En base 2: {int(m1)}. En base 4: {int(m2)}. En base 8: {int(m3)}. En base 16: {int(m4)}") # imprime el numero en su version de la base x

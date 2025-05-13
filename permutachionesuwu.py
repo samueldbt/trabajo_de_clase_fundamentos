1eec3c06249556d4a60dce9fda9e34eb7d98a036
samueldbt <130416036+samueldbt@users.noreply.github.com>
2025-05-13 11:53:32 -0500

    Add files via upload

---
+++ permutachionesuwu.py
@@ -0,0 +1,15 @@
+'''
+a code in where you get a string from the user, then ask for a number N with what
+you make all the combinations posibles with the number N of items of the string at a
+time, showing all this combinations to the user at the end
+'''
+
+# get the string
+string = input("cadena que combinar: ")
+
+# get the number at a time
+n = input("cuantos caracteres a la vez")
+# clean the string
+
+# combinations i guess
+    #show the combinations 
